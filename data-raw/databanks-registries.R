6abe3c1b75406e4d04db30189f15e1616463929e
Maia Salholz-Hillel <mash2015@gmail.com>
2021-02-16 13:40:46 +0100

    Update registries in R code (not csv) to disallow newline \n from non-word \W

--- data-raw/databanks-registries.R
+++ data-raw/databanks-registries.R
@@ -6,14 +6,17 @@ databanks <- readr::read_csv(here("inst", "extdata", "registries.csv"))
 
 databanks <-
   databanks %>%
-  select(-registry_comments, -format_info)
+  dplyr::select(-registry_comments, -format_info)
 
 usethis::use_data(databanks, overwrite = TRUE)
 
 registries <-
   databanks %>%
-  filter(databank_type == "registry") %>%
-  rename(registry = databank)
+  dplyr::filter(databank_type == "registry") %>%
+  dplyr::rename(registry = databank) %>%
+  # dplyr::select(registry, trn_regex) %>%
+  # 2021-02-16: Disallow newline \n from non-word \W
+  dplyr::mutate(trn_regex = stringr::str_replace_all(trn_regex, stringr::coll("\\W"), "[[:blank:][:punct:]]"))
 
 usethis::use_data(registries, overwrite = TRUE)
 
