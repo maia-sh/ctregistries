ba3c1644b3f16b22feffd8757ed3b723e729a8c5
Nachev <vladislav.nachev@charite.de>
2025-04-08 14:11:42 +0200

    add clean parameter to which_trn and which_trns

--- R/which_trn.R
+++ R/which_trn.R
@@ -4,6 +4,8 @@
 #' @param collapse A string. By default, NA, the return is a vector with
 #' all extracted TRNs. If a collapse string is given, then the vector will
 #' be collapsed to a single string with the chosen separator string.
+#' @param clean Logical. By default (TRUE), the individual TRNs will also be
+#' cleaned with `trn_clean`. Skip cleaning by setting to FALSE.
 #'
 #' @return A string (character vector of length number of registries or
 #'  of length 1, depending on value of collapse).
@@ -15,10 +17,12 @@
 #' which_trn("NCT00312962")
 #' which_trn("NCT00312962 and euctr2020-001808-42")
 #' which_trn("NCT00312962 and euctr2020-001808-42", collapse = ";")
+#' which_trn("NCT 00312962 and euctr2020-001808-42", collapse = ";")
+#' which_trn("NCT 00312962 and euctr2020-001808-42", collapse = ";", clean = FALSE)
 #' which_trn("hello")
 #' which_trn(NA, NA)
 
-which_trn <- function(x, collapse = NA_character_) {
+which_trn <- function(x, collapse = NA_character_, clean = TRUE) {
   # # works if only 1 trn
   # stringr::str_extract(x,paste0(ctregistries::registries$trn_regex,
   #                               collapse = "|"))
@@ -36,11 +40,18 @@ which_trn <- function(x, collapse = NA_character_) {
     # Return NA if no trn match
     return(NA_character_)
   } else {
+
+    if (clean == TRUE) {
+      trn <- furrr::future_map_chr(trn,
+                                   \(x) clean_trn(x, quiet = TRUE))
+    }
+
     if (!is.na(collapse)) {
       trn <- paste(trn, collapse = collapse) |>
         dplyr::na_if("NA") |>
         dplyr::na_if("")
     }
+
     return(trn)
   }
 
@@ -53,6 +64,8 @@ which_trn <- function(x, collapse = NA_character_) {
 #' @param collapse A string. By default, NA, the return is a vector with
 #' all extracted TRNs. If a collapse string is given, then the vector will
 #' be collapsed to a single string with the chosen separator string.
+#' @param clean Logical. By default (TRUE), the individual TRNs will also be
+#' cleaned with `trn_clean`. Skip cleaning by setting to FALSE.
 #'
 #' @return A character vector, length of input.
 #'
@@ -68,10 +81,12 @@ which_trn <- function(x, collapse = NA_character_) {
 #' \dontrun{
 #' which_trns(c("NCT00312962 and euctr2020-001808-41", "hello", "euctr2020-001808-42", NA))
 #' }
-#' which_trns(c("NCT00312962 and euctr2020-001808-41", "hello", "euctr2020-001808-42", NA),
+#' which_trns(c("NCT 00312962 and euctr2020-001808-41", "hello", "euctr2020-001808-42", NA),
 #'  collapse = ";")
+#' which_trns(c("NCT 00312962 and euctr2020-001808-41", "hello", "euctr2020-001808-42", NA),
+#'  collapse = ";", clean = FALSE)
 
-which_trns <- function(trn_vec, collapse = NA_character_) {
+which_trns <- function(trn_vec, collapse = NA_character_, clean = TRUE) {
 
   furrr::future_map_chr(trn_vec, \(x) which_trn(x, collapse = collapse), .progress = TRUE)
 }
