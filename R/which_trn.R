4ab8630e8c576b0c035b31dfb42e3a674e0b18bd
Nachev <vladislav.nachev@charite.de>
2025-04-09 12:46:25 +0200

    made output type more consistent for which_trn, improved documentation

--- R/which_trn.R
+++ R/which_trn.R
@@ -1,6 +1,6 @@
 #' Extract TRN(s) from input
 #'
-#' @param x A string (character vector of length 1)
+#' @param x A string (character vector)
 #' @param collapse A string. When a collapse string is given, (by default ";"),
 #' then the return vector will be collapsed to a single string with the chosen
 #' separator string. If collapse is set to "none", the return is a vector with
@@ -21,8 +21,6 @@
 #' which_trn("NCT 00312962 and euctr2020-001808-42")
 #' which_trn("NCT 00312962 and euctr2020-001808-42", clean = FALSE)
 #' which_trn("hello") # return NA if nothing was detected
-#' which_trn(c("NCT 00312962", NA, "", "hello")) # NAs and empty strings are ignored
-#'
 which_trn <- function(x, collapse = ";", clean = TRUE) {
   # # works if only 1 trn
   # stringr::str_extract(x,paste0(ctregistries::registries$trn_regex,
@@ -35,7 +33,8 @@ which_trn <- function(x, collapse = ";", clean = TRUE) {
                                     collapse = "|")#,
                              # simplify = TRUE
     ) |>
-    purrr::flatten_chr()
+    purrr::flatten_chr() |>
+    stats::na.omit()
 
   if (rlang::is_empty(trn)) {
     # Return NA if no trn match
@@ -53,18 +52,20 @@ which_trn <- function(x, collapse = ";", clean = TRUE) {
         dplyr::na_if("") |>
         stats::na.omit() |>
         paste(collapse = collapse)
+      if (trn == "") return(NA_character_)
     }
     return(trn)
   }
-
 }
 
 
 #' Extract TRN(s) from each element of input
 #'
 #' @param trn_vec A character vector.
-#' @param collapse A string. The returned vector will be collapsed to a single
-#' string with the chosen separator string. Cannot be set to NA.
+#' @param collapse A string. If one of the vector elements has multiple TRNS
+#' the element of the output vector will be collapsed to a single
+#' string with the chosen separator string. To make the length of the return
+#' vector the same as the input vector, setting to "none" has been disabled.
 #' @param clean Logical. By default (TRUE), the individual TRNs will also be
 #' cleaned with `trn_clean`. Skip cleaning by setting to FALSE.
 #'
@@ -78,7 +79,7 @@ which_trn <- function(x, collapse = ";", clean = TRUE) {
 #' df <- dplyr::tibble(trn = c("NCT00312962", "hello", "euctr2020-001808-42", NA))
 #' dplyr::mutate(df, trn_extract = which_trns(trn))
 #'
-#' # Does not work for multiple TRNs in one element without collapse
+#' # Cannot set collapse to "none"
 #' \dontrun{
 #' which_trns(c("NCT00312962 and euctr2020-001808-41", "hello", "euctr2020-001808-42", NA),
 #'  collapse = "none")
@@ -86,7 +87,12 @@ which_trn <- function(x, collapse = ";", clean = TRUE) {
 #' which_trns(c("NCT 00312962 and euctr2020-001808-41", "hello", "euctr2020-001808-42", NA))
 #' which_trns(c("NCT 00312962 and euctr2020-001808-41", "hello", "euctr2020-001808-42", NA),
 #'  clean = FALSE)
-#'
+#' # character(0) and NULL in input vector are omitted
+#' # convert NULL values to NAs when importing e.g. with `readr` to preserve
+#' messy_input <- c("", NULL, "Nct02529358", NA, "EudraCT 2004-002714-11", character(0), "NA")
+#' which_trns(messy_input)
+#' # compare to which_trn:
+#' which_trn(messy_input)
 which_trns <- function(trn_vec, collapse = ";", clean = TRUE) {
   stopifnot("Collapse argument cannot be set to 'none'. Did you mean to use which_trn?" = collapse != "none")
   furrr::future_map_chr(trn_vec, \(x) which_trn(x, collapse = collapse, clean = clean), .progress = TRUE)
